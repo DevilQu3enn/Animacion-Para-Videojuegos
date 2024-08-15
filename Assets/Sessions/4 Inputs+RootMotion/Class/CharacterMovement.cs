44556cb042a8194b2fd22dbce3a0678fa864bc96
DevilQu3enn <109630035+DevilQu3enn@users.noreply.github.com>
2024-08-15 17:53:37 -0500

    3PERSON

--- Assets/Sessions/4 Inputs+RootMotion/Class/CharacterMovement.cs
+++ Assets/Sessions/4 Inputs+RootMotion/Class/CharacterMovement.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 using CallbackContext = UnityEngine.InputSystem.InputAction.CallbackContext;
 
 public class CharacterMovement : MonoBehaviour
 { 
     [SerializeField] private Animator anim;
+    [SerializeField] private Transform cameraTransform;
     [SerializeField] private VectorDampener motionVector = new VectorDampener(true);
+    
     private int velXId;
     private int velYId;
     public void Move(CallbackContext ctx)
@@ -29,5 +32,17 @@ public class CharacterMovement : MonoBehaviour
         Vector2 direction = motionVector.CurrentValue;
         anim.SetFloat(velXId, direction.x);
         anim.SetFloat(velYId, direction.y);
+    }
+
+    private void OnAnimatorMove()
+    {
+        float interpolator = Mathf.Abs(Vector3.Dot(cameraTransform.forward, transform.up));
+        
+        Vector3 forward = Vector3.Lerp(cameraTransform.forward,cameraTransform.up,interpolator);
+        Vector3 projected = Vector3.ProjectOnPlane(forward,transform.up);
+        Quaternion rotation = Quaternion.LookRotation(projected, transform.up);
+
+        anim.rootRotation = Quaternion.Slerp(anim.rootRotation, rotation, motionVector.CurrentValue.magnitude);
+        anim.ApplyBuiltinRootMotion();
     }
 }
