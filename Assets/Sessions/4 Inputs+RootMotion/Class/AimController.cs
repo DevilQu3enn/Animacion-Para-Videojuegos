4fbbb4b9e27d14f852505540f39092fc8af5732a
DevilQu3enn <109630035+DevilQu3enn@users.noreply.github.com>
2024-08-15 19:31:52 -0500

    Solucion rotacion-camaraseguimiento-apuntado

--- Assets/Sessions/4 Inputs+RootMotion/Class/AimController.cs
+++ Assets/Sessions/4 Inputs+RootMotion/Class/AimController.cs
@@ -8,30 +8,29 @@ public class AimController : MonoBehaviour
 {
     [SerializeField] private AimConstraint chestAim;
     [SerializeField] private Transform aimRig;
-    [SerializeField] private VectorDampener lookVector;
+    [SerializeField] private Transform camTransform;
+    [SerializeField] private Animator anim;
+
+    private bool aiming;
     public void Aim(InputAction.CallbackContext ctx)
     {
         bool val = ctx.ReadValueAsButton();
+        aiming = val;
 
         //Activar o desactivar el constrait de apuntado del pecho
         chestAim.enabled = val;
 
         //Desencadenar la activacion de la camara de apuntado
         aimRig.gameObject.SetActive(val);
-    }
 
-    public void Look(InputAction.CallbackContext ctx)
-    {
-        lookVector.TargetValue = ctx.ReadValue<Vector2>();
-    }
-
-    private void Update()
-    {
-        lookVector.Update();
-        aimRig.RotateAround(aimRig.position, transform.up, lookVector.CurrentValue.x);
+        anim.SetBool("Aim", val);
     }
     private void Awake()
     {
         aimRig.gameObject.SetActive(false);
+    }
+    private void Update()
+    {
+       if (aiming) transform.rotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(camTransform.forward, transform.up).normalized);
     }
 }
