abeb4625b8eb86316095812a1f0c06bcba3e6ffd
DevilQu3enn <109630035+DevilQu3enn@users.noreply.github.com>
2024-08-20 19:40:06 -0500

    Animationrigg

--- Assets/Sessions/4 Inputs+RootMotion/Class/LookController.cs
+++ Assets/Sessions/4 Inputs+RootMotion/Class/LookController.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class LookController : MonoBehaviour
 {
     [SerializeField] private VectorDampener lookVector;
     [SerializeField] private Transform lookRig;
     [SerializeField] private float sensitivity;
+    [SerializeField] private Vector2 verticalRotationLimits;
+
+    private float rotationy;
     public void Look(InputAction.CallbackContext ctx)
     {
         lookVector.TargetValue = ctx.ReadValue<Vector2>() / new Vector2(Screen.width, Screen.height);
@@ -17,5 +22,10 @@ public class LookController : MonoBehaviour
     {
         lookVector.Update();
         lookRig.RotateAround(lookRig.position, transform.up, lookVector.CurrentValue.x * sensitivity *360f);
+        rotationy -= lookVector.CurrentValue.y * sensitivity * 360f;
+        rotationy = Mathf.Clamp(rotationy, verticalRotationLimits.x, verticalRotationLimits.y);
+        Vector3 euler = lookRig.localEulerAngles;
+        lookRig.localEulerAngles = new Vector3(rotationy, euler.y, euler.z);
+
     }
 }
---
+++ Assets/Sessions/4 Inputs+RootMotion/Class/WeaponFire.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WeaponFire : MonoBehaviour
+{
+    private Animator anim;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Fire(InputAction.CallbackContext ctx)
+    {
+        bool state = ctx.ReadValueAsButton();
+        anim.SetBool("Firing", state);
+    }
+    public void Onshoot()
+    {
+        
+    }
+}
